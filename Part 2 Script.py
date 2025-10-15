835eb78eb090460a0e94e9f72afbee5f2c9e7d6b
Chris-Winnard <74295248+Chris-Winnard@users.noreply.github.com>
2025-10-15 15:50:25 +0100

    Add files via upload

--- Part 2 Script.py
+++ Part 2 Script.py
@@ -2,7 +2,7 @@
 
 from psychopy import locale_setup
 from psychopy import sound, gui, visual, core, data, event, logging, clock, colors, layout, prefs
-prefs.hardware['audioLatencyMode'] = '3'
+#prefs.hardware['audioLatencyMode'] = '3'  Only relevant if using PTB sound backend
 prefs.hardware['audioDevice'] = 'Speakers (DAC8PRO)'
 from psychopy.constants import (NOT_STARTED, STARTED, PLAYING, PAUSED,
                                 STOPPED, FINISHED, PRESSED, RELEASED, FOREVER)
